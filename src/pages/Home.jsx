0463f800fc9ec217896dc1c2f8753b642705cb04
Gustavo Andres Santoyo Benavides <150487562+DocGus@users.noreply.github.com>
2025-04-18 15:02:09 +0000

    Personajes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { fechtPersonajes } from "../store.js";
+import { CardPersonaje } from "../components/CardPersonaje.jsx";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
-
+useEffect(()=>{
+fechtPersonajes(dispatch)
+},[])
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<h1>Star Wars</h1>
+			<div className="d-flex flex-row overflow-scroll">
+			{store.personajes.map((personaje)=>(
+				// <p>{personaje.name}</p>
+				<CardPersonaje
+				key={personaje.uid}
+				id={personaje.uid}
+				nombre={personaje.name}
+				/>
+			))}
+			</div>
 		</div>
 	);
 }; 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
+    personajes:[],
     message: null,
     todos: [
       {
@@ -17,16 +18,36 @@ export const initialStore=()=>{
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
+  switch (action.type) {
     case 'add_task':
 
-      const { id,  color } = action.payload
+      const { id, color } = action.payload
 
       return {
         ...store,
         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
       };
+      case "personajes":
+        return{
+        ...store,
+        personajes:action.payload
+      }
     default:
       throw Error('Unknown action.');
-  }    
+  }
+}
+
+export const fechtPersonajes = async (dispatch) => {
+  try {
+    const response = await fetch("https://www.swapi.tech/api/people")
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    dispatch({
+      type:"personajes", 
+      payload:data.results
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
